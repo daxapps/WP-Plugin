35efb06d08b5cdb225fdbe57b683ef3a587c5b26
Jason Crawford <daxapps777@gmail.com>
2018-03-02 13:29:56 -0700

    Build form with shortcode

--- wp-content/plugins/mail-gorilla/mail-gorilla.php
+++ wp-content/plugins/mail-gorilla/mail-gorilla.php
@@ -11,3 +11,97 @@ License URI:  https://www.gnu.org/licenses/gpl-2.0.html
 Text Domain:  
 Domain Path:  /languages
 */
+
+/* !0. TABLE OF CONTENTS */
+
+/*
+	
+  1. HOOKS
+  	1.1 - registers all our custom shortcodes
+	
+	2. SHORTCODES
+		2.1 - mgr_register_shortcodes()
+		2.2 - mgr_form_shortcode()
+		
+	3. FILTERS
+		
+	4. EXTERNAL SCRIPTS
+		
+	5. ACTIONS
+		
+	6. HELPERS
+		
+	7. CUSTOM POST TYPES
+	
+	8. ADMIN PAGES
+	
+	9. SETTINGS
+
+*/
+
+/* !1. HOOKS */
+
+// 1.1
+// hint: registers all our custom shortcodes on init
+add_action('init', 'mgr_register_shortcodes');
+
+/* !2. SHORTCODES */
+
+// 2.1
+// hint: registers all our custom shortcodes
+function mgr_register_shortcodes() {
+	
+	add_shortcode('mgr_form', 'mgr_form_shortcode');
+	
+}
+
+// 2.2
+// hint: returns a html string for a email capture form
+function mgr_form_shortcode( $args, $content="") {
+	
+	// setup our output variable - the form html 
+	$output = '
+	
+		<div class="mgr">
+		
+			<form id="mgr_form" name="mgr_form" class="mgr-form" method="post">
+			
+				<p class="mgr-input-container">
+				
+					<label>Your Name</label><br />
+					<input type="text" name="mgr_fname" placeholder="First Name" />
+					<input type="text" name="mgr_lname" placeholder="Last Name" />
+				
+				</p>
+				
+				<p class="mgr-input-container">
+				
+					<label>Your Email</label><br />
+					<input type="email" name="mgr_email" placeholder="ex. you@email.com" />
+				
+				</p>';
+				
+				// including content in our form html if content is passed into the function
+				if( strlen($content) ):
+				
+					$output .= '<div class="mgr-content">'. wpautop($content) .'</div>';
+				
+				endif;
+				
+				// completing our form html
+				$output .= '<p class="mgr-input-container">
+				
+					<input type="submit" name="mgr_submit" value="Sign Me Up!" />
+				
+				</p>
+			
+			</form>
+		
+		</div>
+	
+	';
+	
+	// return our results/html
+	return $output;
+	
+}
