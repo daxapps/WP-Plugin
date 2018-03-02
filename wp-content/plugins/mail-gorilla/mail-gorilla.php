8dbf4f8c0bc246688cc9fb60cf250e6aecdb6887
Jason Crawford <daxapps777@gmail.com>
2018-03-02 13:05:30 -0700

    Setup plugin WP header

---
+++ wp-content/plugins/mail-gorilla/mail-gorilla.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Plugin Name: Mail Gorilla
+Plugin URI:
+Description:  The best email collector
+Version:      1.0
+Author:       DaxApps
+Author URI:   https://developerdax.com/
+License:      GPL2
+License URI:  https://www.gnu.org/licenses/gpl-2.0.html
+Text Domain:  
+Domain Path:  /languages
+*/
